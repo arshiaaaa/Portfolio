bc23ea56c1c02bbb6d1f106f87d6bea1948a870d
Arshia <arshiasandhu07@gmail.com>
2024-03-04 18:51:11 -0500

    Changes

--- home.js
+++ home.js
@@ -30,4 +30,10 @@ $(document).ready(function () {
       }
     }
   });
+  $(".option").click(function(){
+    $(".option").removeClass("active");
+    $(this).addClass("active");
+    
+ });
+ 
 });
